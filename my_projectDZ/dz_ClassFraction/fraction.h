b98c02fa25cfdc037d02fb567423e9a4eb3da6c8
vactor777 <64753907+vactor777@users.noreply.github.com>
2021-12-21 03:21:03 +0700

    Create fraction.h

---
+++ my_projectDZ/dz_ClassFraction/fraction.h
@@ -0,0 +1,17 @@
+#ifndef FRACT_0
+#define FRACT_0
+#include <iostream>
+class Fraction
+{
+private:
+    int numerator;
+    int denominator;
+public:
+    void enter_numenator();
+    void enter_denominator();
+    void show_drob();
+    void addition_drob(double n);
+    void substraction_drob(double n);
+    void mu_drob(double n);
+};
+#endif
